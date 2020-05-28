8bb9fa2eebfeeca8314470453380824c6dff1e3b
valentino <valentino.digiosaffatte@student.univaq.it>
2020-05-28 16:25:03 +0200

    navigation drawer and toolba

--- app/src/main/java/it/univaq/disim/mwt/android_native_app/MainActivity.java
+++ app/src/main/java/it/univaq/disim/mwt/android_native_app/MainActivity.java
@@ -1,21 +1,35 @@
 package it.univaq.disim.mwt.android_native_app;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private ExploreTvShowsPopularFragment exploreTvShowsPopularFragment;
+    private ExploreTvShowsTopRatedFragment exploreTvShowsTopRatedFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +53,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+        viewPager = findViewById(R.id.main_viewpager);
+        tabLayout = findViewById(R.id.main_tab_layout);
+
+        exploreTvShowsPopularFragment = new ExploreTvShowsPopularFragment();
+        exploreTvShowsTopRatedFragment = new ExploreTvShowsTopRatedFragment();
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), 0);
+        viewPagerAdapter.addFragment(exploreTvShowsPopularFragment, "popular");
+        viewPagerAdapter.addFragment(exploreTvShowsTopRatedFragment, "top rated");
+        viewPager.setAdapter(viewPagerAdapter);
+
+        //tabLayout.getTabAt(0).setIcon(R.drawable.ic_explore_black_24dp);
     }
 
     @Override
@@ -49,5 +78,37 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragmentList = new ArrayList<>();
+        private List<String> fragmentListTitles = new ArrayList<>();
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentListTitles.add(title);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentListTitles.get(position);
+        }
     }
 }
