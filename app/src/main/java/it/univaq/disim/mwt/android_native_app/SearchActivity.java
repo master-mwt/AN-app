4189949e7e753a7f5a4e0879f51f3be0277bba6b
ricdip <riccardodpr@gmail.com>
2020-06-09 19:46:06 +0200

    search and collection drawer, collection activity, fixes

--- app/src/main/java/it/univaq/disim/mwt/android_native_app/SearchActivity.java
+++ app/src/main/java/it/univaq/disim/mwt/android_native_app/SearchActivity.java
@@ -5,12 +5,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -19,8 +25,11 @@ import it.univaq.disim.mwt.android_native_app.api.TMDB;
 import it.univaq.disim.mwt.android_native_app.model.TvShowPreview;
 import it.univaq.disim.mwt.android_native_app.services.DataParserService;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     private ArrayList<TvShowPreview> data = new ArrayList<>();
     private RecyclerViewTvShowCardAdapter recyclerViewTvShowCardAdapter;
     private RecyclerView recyclerView;
@@ -47,6 +56,20 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        toolbar = findViewById(R.id.search_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        drawerLayout = findViewById(R.id.main_drawer_layout);
+        navigationView = findViewById(R.id.main_navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
 
         recyclerView = findViewById(R.id.search_tv_shows_recycle_view);
     }
@@ -83,5 +106,34 @@ public class SearchActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent intent = null;
+        switch (item.getItemId()){
+            case R.id.menu_item_search:
+                drawerLayout.closeDrawers();
+                break;
+            case R.id.menu_item_explore:
+                drawerLayout.closeDrawers();
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_collection:
+                drawerLayout.closeDrawers();
+                intent = new Intent(this, UserCollectionActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
--- app/src/main/java/it/univaq/disim/mwt/android_native_app/UserCollectionActivity.java
+++ app/src/main/java/it/univaq/disim/mwt/android_native_app/UserCollectionActivity.java
@@ -1,14 +1,96 @@
 package it.univaq.disim.mwt.android_native_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class UserCollectionActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.univaq.disim.mwt.android_native_app.model.TvShowPreview;
+
+public class UserCollectionActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private RecyclerView recyclerView;
+    private TextView emptyList;
+    private List<TvShowPreview> collection = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_collection);
+
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        drawerLayout = findViewById(R.id.main_drawer_layout);
+        navigationView = findViewById(R.id.main_navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        recyclerView = findViewById(R.id.collection_tv_shows_recycle_view);
+
+        emptyList = findViewById(R.id.empty_list);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(collection.isEmpty()){
+            recyclerView.setVisibility(View.INVISIBLE);
+            emptyList.setVisibility(View.VISIBLE);
+        } else {
+            recyclerView.setVisibility(View.VISIBLE);
+            emptyList.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent intent = null;
+        switch (item.getItemId()){
+            case R.id.menu_item_search:
+                drawerLayout.closeDrawers();
+                intent = new Intent(this, SearchActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_explore:
+                drawerLayout.closeDrawers();
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_collection:
+                drawerLayout.closeDrawers();
+                break;
+            default:
+                break;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
