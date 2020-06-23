7a76a72ac01f2b017ac2f761ecf5d5e13bab80e9
ricdip <riccardodpr@gmail.com>
2020-06-23 19:09:47 +0200

    Geolocation (Google Play Services version)

--- app/src/main/java/it/univaq/disim/mwt/android_native_app/InfoActivity.java
+++ app/src/main/java/it/univaq/disim/mwt/android_native_app/InfoActivity.java
@@ -1,24 +1,44 @@
 package it.univaq.disim.mwt.android_native_app;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-// TODO: geolocation
+import it.univaq.disim.mwt.android_native_app.dialogs.PositionPermissionDeniedDialogFragment;
+import it.univaq.disim.mwt.android_native_app.utils.LocationPermission;
+
 public class InfoActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    // TODO: Change variables name
+    // TODO: Change variables name (logo, app description fields (?))
     private TextView textView1;
     private TextView textView2;
     private GoogleMap map;
+    private Marker marker;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +46,10 @@ public class InfoActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_info);
 
         textView1 = findViewById(R.id.textView2);
-        textView1.setText("Testo 1");
+        textView1.setText("logo app");
 
         textView2 = findViewById(R.id.textView3);
-        textView2.setText("Testo 2");
+        textView2.setText("Descrizione");
 
         SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         if(supportMapFragment != null){
@@ -41,14 +61,93 @@ public class InfoActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
 
-        // TODO: logic
-        LatLng position = new LatLng(41.902782, 12.496366);
+        // Universita degli Studi dell'Aquila
+        LatLng position = new LatLng(42.36779, 13.352897);
 
         MarkerOptions options = new MarkerOptions();
-        options.title("Marker");
+        options.title("Università degli Studi dell'Aquila");
         options.position(position);
-        map.addMarker(options);
+        marker = map.addMarker(options);
 
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 12f));
+
+        if(LocationPermission.isLocationPermissionGranted(this)){
+            // geolocation
+            getUserPosition();
+        } else {
+            LocationPermission.requestLocationPermission(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == LocationPermission.REQUEST_PERMISSION_CODE){
+            if(LocationPermission.isLocationPermissionGranted(this)){
+                // geolocation
+                getUserPosition();
+            } else {
+                // permissions not granted
+                showPermissionNotGrantedDialog();
+            }
+        }
+    }
+
+    private void getUserPosition(){
+        if(GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this) == ConnectionResult.SUCCESS){
+            // Google Play Services available
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+            fusedLocationProviderClient.getLocationAvailability().addOnCompleteListener(new OnCompleteListener<LocationAvailability>() {
+                @Override
+                public void onComplete(@NonNull Task<LocationAvailability> task) {
+                    LocationAvailability locationAvailability = task.getResult();
+
+                    if(locationAvailability == null || !(locationAvailability.isLocationAvailable())){
+                        // location not available already
+                        LocationRequest locationRequest = new LocationRequest();
+                        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest, new LocationCallback(){
+                            @Override
+                            public void onLocationResult(LocationResult locationResult) {
+                                super.onLocationResult(locationResult);
+
+                                setUserLocation(locationResult.getLastLocation());
+                                fusedLocationProviderClient.removeLocationUpdates(this);
+                            }
+                        }, Looper.getMainLooper());
+                    } else {
+                        // location available already
+                        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                setUserLocation(location);
+                            }
+                        });
+                    }
+
+                }
+            });
+        }
+    }
+
+    private void setUserLocation(Location location){
+        if(location != null){
+            LatLng userPosition = new LatLng(location.getLatitude(), location.getLongitude());
+
+            MarkerOptions options = new MarkerOptions();
+            options.title("current user");
+            options.position(userPosition);
+            map.addMarker(options);
+
+            LatLngBounds.Builder boundsBuilder = new LatLngBounds.Builder();
+            boundsBuilder.include(userPosition).include(marker.getPosition());
+
+            map.moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(), 100));
+        }
+    }
+
+    private void showPermissionNotGrantedDialog(){
+        PositionPermissionDeniedDialogFragment fragment = new PositionPermissionDeniedDialogFragment();
+        fragment.show(getSupportFragmentManager(), "position_denied_dialog");
     }
 }
---
+++ app/src/main/java/it/univaq/disim/mwt/android_native_app/utils/LocationPermission.java
@@ -0,0 +1,22 @@
+package it.univaq.disim.mwt.android_native_app.utils;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class LocationPermission {
+
+    public static final int REQUEST_PERMISSION_CODE = 100;
+
+    public static boolean isLocationPermissionGranted(Context context) {
+        return ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static void requestLocationPermission(Activity activity){
+        ActivityCompat.requestPermissions(activity, new String[]{ Manifest.permission.ACCESS_FINE_LOCATION }, REQUEST_PERMISSION_CODE);
+    }
+}
